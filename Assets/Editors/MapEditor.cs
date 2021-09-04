3194583255c8229f3886d2273058fd54c90f62ec
DESKTOP-GG6IUDJ\enes_ <yslyrt.enes@gmail.com>
2021-09-04 23:59:55 +0300

    map editor

---
+++ Assets/Editors/MapEditor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEditor;
+using Assets.Scripts;
+
+namespace Assets.Editors
+{
+    [CustomEditor(typeof(MapGenerator))]
+    public class MapEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            MapGenerator map = target as MapGenerator;
+            map.GenerateMap();
+        }
+    }
+}
