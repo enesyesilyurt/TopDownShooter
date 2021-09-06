9e13e09901b88fcb03702b3393807691c3321688
DESKTOP-GG6IUDJ\enes_ <yslyrt.enes@gmail.com>
2021-09-06 23:59:35 +0300

    map generator

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -10,9 +10,13 @@ namespace Assets.Scripts
         public Vector2 mapSize;
         [Range(0, 1)]
         public float outlinePercent;
+        [Range(0, 1)]
+        public float obstaclePercent;
+
         List<Coord> allTileCoords;
         Queue<Coord> shuffledTileCoords;
         public int seed = 10;
+        Coord mapCentre;
         private void Start()
         {
             GenerateMap();
@@ -29,6 +33,7 @@ namespace Assets.Scripts
                 }
             }
             shuffledTileCoords = new Queue<Coord>(Utility.ShuffleArray(allTileCoords.ToArray(),seed));
+            mapCentre = new Coord((int)mapSize.x / 2, (int)mapSize.y / 2);
             string holderName = "Generated Map";
             if (transform.Find(holderName))
             {
@@ -42,22 +47,70 @@ namespace Assets.Scripts
             {
                 for (int y = 0; y < mapSize.y; y++)
                 {
-                    Vector3 tilePosition = CoorToPosition(x, y);
+                    Vector3 tilePosition = CoordToPosition(x, y);
                     Transform newTile = Instantiate(tilePrefab, tilePosition, Quaternion.Euler(Vector3.right * 90)) as Transform;
                     newTile.localScale = Vector3.one * (1 - outlinePercent);
                     newTile.parent = mapHolder;
                 }
             }
-            int obstacleCount = 10;
+
+            bool[,] obstacleMap = new bool[(int)mapSize.x, (int)mapSize.y];
+
+            int obstacleCount = (int)(mapSize.x*mapSize.y* obstaclePercent);
+            int currentObstacleCount = 0;
             for (int i = 0; i < obstacleCount; i++)
             {
                 Coord randomCoord = GetRandomCoord();
-                Vector3 obstaclePosition = CoorToPosition(randomCoord.x, randomCoord.y);
-                Transform newObstacle = Instantiate(obstaclePrefab, obstaclePosition+Vector3.up*.5f, Quaternion.identity) as Transform;
-                newObstacle.parent = mapHolder;
+                obstacleMap[randomCoord.x, randomCoord.y] = true;
+                currentObstacleCount++;
+                if (randomCoord != mapCentre && MapIsFullyAccessible(obstacleMap, currentObstacleCount))
+                {
+
+
+                    Vector3 obstaclePosition = CoordToPosition(randomCoord.x, randomCoord.y);
+
+                    Transform newObstacle = Instantiate(obstaclePrefab, obstaclePosition + Vector3.up * .5f, Quaternion.identity) as Transform;
+                    newObstacle.parent = mapHolder;
+                }
+                else
+                {
+                    obstacleMap[randomCoord.x, randomCoord.y] = false;
+                    currentObstacleCount--;
+
+                }
             }
         }
-        Vector3 CoorToPosition(int x,int y)
+        bool MapIsFullyAccessible(bool[,] obstacleMap,int currentObstacleCount)
+        {
+            bool[,] mapFlags = new bool[obstacleMap.GetLength(0), obstacleMap.GetLength(1)];
+            Queue<Coord> queue=new Queue<Coord>();
+            queue.Enqueue(mapCentre);
+            mapFlags[mapCentre.x, mapCentre.y] = true;
+            while (queue.Count>0)
+            {
+                Coord tile = queue.Dequeue();
+                for (int x = -1; x <= 1; x++)
+                {
+                    for (int y = -1; y <= 1; y++)
+                    {
+                        int neighbourX = tile.x + x;
+                        int neighbourY = tile.y + y;
+                        if (x == 0 || y == 0)
+                        {
+                            if (neighbourX >= 0 && neighbourX < obstacleMap.GetLength(0) && neighbourY >= 0 && neighbourY < obstacleMap.GetLength(1))
+                            {
+                                if (!mapFlags[neighbourX, neighbourY] && !obstacleMap[neighbourX, neighbourY])
+                                {
+                                    mapFlags[neighbourX, neighbourY] = true;
+                                    queue.Enqueue(new Coord(neighbourX, neighbourY));
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        Vector3 CoordToPosition(int x,int y)
         {
             return new Vector3(-mapSize.x / 2 + 0.5f + x, 0, -mapSize.y + 0.5f + y);
         }
